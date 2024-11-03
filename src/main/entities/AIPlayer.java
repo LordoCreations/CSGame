06ccc62eafdef8500ead53846d4326083d3d3f77
LordoCreations <lordocreations@gmail.com>
2024-11-03 10:14:48 -0800

    minor bug fix on horizontal

--- src/main/entities/AIPlayer.java
+++ src/main/entities/AIPlayer.java
@@ -32,7 +32,7 @@ public class AIPlayer extends Player {
             input.remove(KeyEvent.VK_UP);
         }
 
-        horizontalThreshold = Math.min(input.contains(KeyEvent.VK_UP) || Math.abs(theirCoord[1] - myCoord[1]) < 50 ? 0 : 300, weapon.getFiringDistance() / 2.0);
+        horizontalThreshold = Math.min(input.contains(KeyEvent.VK_UP) || Math.abs(theirCoord[1] - myCoord[1]) > 50 ? 0 : 300, weapon.getFiringDistance() / 2.0);
 
         if (theirCoord[0] - myCoord[0] > horizontalThreshold) {
             input.remove(KeyEvent.VK_LEFT);
